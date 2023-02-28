77ab0fccdb876c73e926c957c9a56a953a53c201
SpookyLittleGhost <felix.evers.web@gmail.com>
2023-02-28 19:07:52 +0100

    check

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
-def calc_time_for_gunmarks(alpha, reload, gunmarks):
+def calc_time_for_gunmarks(tank):
+
+    gun = tank[0]
+    alpha = tank[1]
+    reload = tank[2]
+    gunmarks = tank[3]
 
     alphas = [alpha * 0.75, alpha, alpha * 1.25]
 
     times_needed = []
 
-    i = 0
+    if gun == "single":
 
-    for gm in gunmarks:
-        counter = 0
-        j = 0
+        for gm in gunmarks:
 
-        for alpha in alphas:
+            times = []
 
-            done = alpha
-            time = 0
+            for alpha in alphas:
 
-            while done < gm:
-                time += reload
-                done += alpha
+                deal = []
 
-            shots = time / reload
+                done = alpha
+                time = 0
 
-            print(f"Inspected gunmark: {gm} for Alpha {alpha} needs {time} seconds equal to {shots} shots!")
+                while done < gm:
+                    time += reload
+                    done += alpha
 
-            times_needed.append(time)
+                shots = time / reload
 
-            j += 1
+                print(f"Inspected gunmark: {gm} for Alpha {alpha} needs {time} seconds equal to {shots} shots!")
 
-        i += 1
+                deal.append(time)
+                deal.append(shots)
+
+                times.append(deal)
+
+            times_needed.append(times)
+    elif gun == "magazine":
+        print("is magazine")
+    elif gun == "autoreload":
+        print("is autoreload")
+    else:
+        print(f"unknown gun type {gun}")
 
     return times_needed
 
 
 # The main method
-alpha = 490
-reload = 12.5
-gunmarks = [2500, 3000, 3750]
+tank1 = ["single", 490, 12.5, [2500, 3000, 3750]]
+tank2 = ["magazine", 280, 3, [2500, 3000, 3750], 4, 38.5]
+tank3 = ["autoreload", 280, 2.5, [2500, 3000, 3750], 3, [10.5, 8.7, 6.5]]
 
-times = calc_time_for_gunmarks(alpha, reload, gunmarks)
+tank1.append(calc_time_for_gunmarks(tank1))
+tank2.append(calc_time_for_gunmarks(tank2))
+tank3.append(calc_time_for_gunmarks(tank3))
 
-print(times)
+print(tank1)
+print(tank2)
+print(tank3)
